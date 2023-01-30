c7453851354cba6f95a975762fc15428e8429f3d
Sergei US <ustimenko.sv@gmail.com>
2023-01-30 20:49:43 +0300

    Initial commit

---
+++ cypress/e2e/apitests.cy.js
@@ -0,0 +1,27 @@
+const { describe } = require("mocha");
+import { username, firstName, email , password, newUserName } from '../fixtures/variables.json'
+
+describe('tests ipi', () => {
+  it('should create a new user', () => {
+    cy.createUser(username, firstName, email, password)
+      .then(response => {
+        expect(response.status).equal(200)
+    })
+  })
+
+  it('should update a user name', () => {
+    cy.createUser(username, firstName, email, password)
+    cy.updateUser(newUserName)
+      .then(response => {
+        expect(response.status).equals(200)
+    }) 
+  })
+
+  it('should delete a user', () => {
+    // cy.createUser(username, firstName, email, password)
+    cy.deleteUser(username)
+      .then(response => {
+        expect(response.status).equals(200)
+    }) 
+  })
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,63 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import { username } from '../fixtures/variables.json'
+
+Cypress.Commands.add('createUser', (username, firstName, email, password) => {
+    cy.request({
+        url: '/',
+        method: 'POST',
+        body: 
+        {
+            username,
+            firstName,
+            email,
+            password
+        }
+    })
+})
+
+Cypress.Commands.add('updateUser', (newUserName) => {
+    cy.request({
+        url: `/${username}`,
+        method: 'PUT',
+        body: 
+        {
+            newUserName
+        }
+    })
+})
+
+Cypress.Commands.add('deleteUser', (username) => {
+    cy.request({
+        url: `/${username}`,
+        method: 'DELETE',
+        body: 
+        {
+            username
+        }
+    })
+})
